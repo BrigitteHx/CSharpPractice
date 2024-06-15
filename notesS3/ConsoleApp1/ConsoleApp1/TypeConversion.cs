4b956c6a7ce610ada7639591e745aa9c71a457f3
BrigitteHx <brigitteheijkoop@gmail.com>
2024-06-15 11:08:52 +0200

    herhaling basics samengevat

---
+++ notesS3/ConsoleApp1/ConsoleApp1/TypeConversion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            //int i = 1;
+            //byte b = (byte)i;
+            //Console.WriteLine(b);
+
+            //var number = "1234";
+            //int i = Convert.ToInt32(number);
+            //Console.WriteLine(i);
+
+            //try
+            //{
+            //    var number2 = "1234";
+            //    byte b = Convert.ToByte(number2);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted.");
+            //}
+
+
+            try
+            {
+                string str = "true"; 
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not be converted.");
+            }
+
+        }
+    }
+}
---
+++ notesS3/ConsoleApp1/ConsoleApp1/Variables.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+//namespace Variables
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            byte number = 2;
+//            int count = 10;
+//            float totalPrice = 20.95f;
+//            char character = 'A';
+//            string firstName = "Brigitte";
+//            bool isWorking = true;
+
+//            Console.WriteLine(number);
+//            Console.WriteLine(count);
+//            Console.WriteLine(totalPrice);
+//            Console.WriteLine(character);
+//            Console.WriteLine(firstName);
+//            Console.WriteLine(isWorking);
+//        }
+//    }
+//}
+
+//namespace Variables
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var number = 2;
+//            var count = 10;
+//            var totalPrice = 20.95f;
+//            var character = 'A';
+//            var firstName = "Brigitte";
+//            var isWorking = true;
+
+//            Console.WriteLine(number);
+//            Console.WriteLine(count);
+//            Console.WriteLine(totalPrice);
+//            Console.WriteLine(character);
+//            Console.WriteLine(firstName);
+//            Console.WriteLine(isWorking);
+//        }
+//    }
+//}
+
+//namespace Variables
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+//            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+//        }
+//    }
+//}
+
